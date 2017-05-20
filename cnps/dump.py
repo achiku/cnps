8beb4ed2753828e7d3d85f4dcd23feab97db0e7d
Akira Chiku <akira.chiku@gmail.com>
2017-05-20 16:52:33 +0900

    Add find_user_details

--- cnps/dump.py
+++ cnps/dump.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import click
+from datetime import datetime
 
 
 def find_user_urls(soup):
@@ -9,3 +9,32 @@ def find_user_urls(soup):
     for u in users:
         user_urls.append(u['href'])
     return user_urls
+
+
+def find_user_details(soup, user_url):
+    event_dates = []
+    event_dates_soup = soup.find_all('div', class_='event_schedule_area')
+    for e in event_dates_soup:
+        year = e.find('p', class_='year').text
+        date = e.find('p', class_='date').text
+        dt = datetime.strptime("{0}/{1}".format(year, date), '%Y/%m/%d')
+        event_dates.append(dt)
+
+    social_links = []
+    social_links_soup = soup.find('span', class_='social_link').find_all('a')
+    for link in social_links_soup:
+        url = link['href']
+        if 'twitter' in url:
+            social_links.append({'twitter': url})
+        elif 'facebook' in url:
+            social_links.append({'facebook': url})
+        elif 'github' in url:
+            social_links.append({'github': url})
+
+    user_id = user_url.strip('https://connpass.com/user/')
+    return {
+        'user_url': user_url,
+        'user_id': user_id,
+        'event_dates': event_dates,
+        'social_links': social_links,
+    }
--- tests/test_dump.py
+++ tests/test_dump.py
@@ -13,14 +13,9 @@ def test_find_user_urls():
 
 
 def test_find_user_detail():
+    from cnps.dump import find_user_details
     with open('./data/connpass_user_detail.html') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
-        event_date = soup.find_all('div', class_='event_schedule_area')
-        for e in event_date:
-            year = e.find('p', class_='year').text
-            date = e.find('p', class_='date').text
-            print("{0}/{1}".format(year, date))
-        social_links = soup.find('span', class_='social_link').find_all('a')
-        for i in social_links:
-            print(i['href'])
+        user = find_user_details(soup, 'https://connpass.com/user/KeisukeKogure/')
+        print(user)
     assert False
